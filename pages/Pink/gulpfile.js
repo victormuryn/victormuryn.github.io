9c3adbdd456ce4c877497dc9de4423d1b6171b69
Victor <victormuryn24@uk.net>
2018-08-04 15:11:58 +0300

    Checkpoint

--- pages/Pink/gulpfile.js
+++ pages/Pink/gulpfile.js
@@ -1,26 +1,37 @@
 var
-	gulp = require("gulp"),
-	livereload = require("gulp-livereload"),
-	sass = require("gulp-sass"),
+	gulp         = require("gulp"),
+	livereload   = require("gulp-livereload"),
+	sass         = require("gulp-sass"),
 	autoprefixer = require("gulp-autoprefixer"),
-	cleancss = require("gulp-clean-css"),
-	rename = require("gulp-rename");
+	cleancss     = require("gulp-clean-css"),
+	rename       = require("gulp-rename");
 
 gulp.task("reload-css", function() {
- gulp.src('./src/*.scss')
- .pipe(sass().on("error", sass.logError))
- .pipe(autoprefixer({
- 		browsers: ["last 3 version"],
- 		cascade: false
- 	}))
- .pipe(gulp.dest("./src/css/"))
- .pipe(cleancss({compatibility: "ie8"}))
- .pipe(rename({suffix: ".min"}))
- .pipe(gulp.dest("./src/css/"))
- .pipe(livereload());
+	gulp.src('./src/sass/style.scss')
+		.pipe(sass().on("error", sass.logError))
+		.pipe(autoprefixer({
+			browsers: ["last 3 version"],
+			cascade: false
+		}))
+		.pipe(gulp.dest("./src/css/"))
+		.pipe(cleancss({ compatibility: "ie8" }))
+		.pipe(rename({ suffix: ".min" }))
+		.pipe(gulp.dest("./src/css/"))
+		.pipe(livereload());
 });
 
+gulp.task("reload-html", function() {
+	gulp.src('./src/*.html').pipe(livereload());
+});
+
+//gulp.task("reload-js", function() {
+//gulp.src('./src/js/*.js').pipe(livereload());
+//});
+
 gulp.task("default", function() {
- livereload.listen();
-	gulp.watch('./src/*.scss', ['reload-css']);
-}); ﻿
+	livereload.listen();
+	gulp.watch('./src/sass/*.scss', ['reload-css']);
+	gulp.watch('./src/sass/blocks/*.scss', ['reload-css']);
+	gulp.watch('./src/*.html', ['reload-html']);
+	//gulp.watch('./src/js/*.js', ['reload-js']);
+});﻿
