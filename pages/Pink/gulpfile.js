eeea814cec34a0f42ccca73d858efafbd3450444
Victor <victormuryn24@uk.net>
2018-08-15 13:44:24 +0300

    checkpoint

--- pages/Pink/gulpfile.js
+++ pages/Pink/gulpfile.js
@@ -1,39 +1,54 @@
 var
-	gulp         = require("gulp"),
-	livereload   = require("gulp-livereload"),
-	sass         = require("gulp-sass"),
-	autoprefixer = require("gulp-autoprefixer"),
-	csscomb      = require("gulp-csscomb");
-	cleancss     = require("gulp-clean-css"),
-	rename       = require("gulp-rename");
+	gulp = require("gulp"),
+	sass = require("gulp-sass"),
+	plumber = require("gulp-plumber"),
+	postcss = require("gulp-postcss"),
+	csscomb = require("gulp-csscomb"),
+	autoprefixer = require("autoprefixer"),
+	minify = require("gulp-csso"),
+	imagemin = require("gulp-imagemin"),
+	webp = require("gulp-webp"),
+	server = require("browser-sync").create(),
+	rename = require("gulp-rename");
 
-gulp.task("reload-css", function() {
-	gulp.src('./src/sass/style.scss')
+gulp.task("style", function() {
+	gulp.src("src/sass/style.scss")
+		.pipe(plumber())
 		.pipe(sass().on("error", sass.logError))
-		.pipe(autoprefixer({
-			browsers: ["last 3 version"],
-			cascade: false
-		}))
+		.pipe(postcss([autoprefixer()]))
 		.pipe(csscomb())
-		.pipe(gulp.dest("./src/css/"))
-		.pipe(cleancss({ compatibility: "ie8" }))
-		.pipe(rename({ suffix: ".min" }))
-		.pipe(gulp.dest("./src/css/"))
-		.pipe(livereload());
+		.pipe(gulp.dest("src/css"))
+		.pipe(minify())
+		.pipe(rename("style.min.css"))
+		.pipe(gulp.dest("src/css"))
+		.pipe(server.stream());
 });
 
-gulp.task("reload-html", function() {
-	gulp.src('./src/*.html').pipe(livereload());
+gulp.task("images", function() {
+	return gulp.src("src/img/**/*.{png,jpg,svg}")
+		.pipe(imagemin([imagemin.optipng({ optimizationLevel: 3 }),
+			imagemin.jpegtran({ progressive: true }),
+			imagemin.svgo()
+		]))
+		.pipe(gulp.dest("src/img"))
+		.pipe(server.stream());
 });
 
-gulp.task("reload-js", function() {
-gulp.src('./src/js/*.js').pipe(livereload());
+gulp.task("webp", function() {
+	return gulp.src("src/img/**/*.{png,jpg}")
+		.pipe(webp({ quality: 90 }))
+		.pipe(gulp.dest("src/img"))
+		.pipe(server.stream());
 });
 
-gulp.task("default", function() {
-	livereload.listen();
-	gulp.watch('./src/sass/*.scss', ['reload-css']);
-	gulp.watch('./src/sass/blocks/*.scss', ['reload-css']);
-	gulp.watch('./src/*.html', ['reload-html']);
-	gulp.watch('./src/js/*.js', ['reload-js']);
-});﻿
+gulp.task("default", ["style"], function() {
+	server.init({
+		server: "src/"
+	});
+
+	gulp.watch("src/sass/**/*.scss", ["style"]);
+	gulp.watch("src/*.html").on("change", server.reload);
+	gulp.watch("src/js/*.js").on("change", server.reload);
+	/* gulp.watch("src/img/*.{png,jpg}", ["webp"]);
+	 gulp.watch("src/img/*.{png,jpg,svg}", ["images"]); */
+});
