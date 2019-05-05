07a01d444e4ac62a7e097a80c8073afd2fa707c1
Victor <victormuryn24@gmail.com>
2019-05-05 15:38:03 +0300

    mishka tavlet index is ready

--- pages/Mishka/src/js/script.js
+++ pages/Mishka/src/js/script.js
@@ -1,7 +1,10 @@
 let nav = document.querySelector(".navigation"),
 toggle = document.querySelector(".header__menu-toggle"),
 close = document.querySelector(".header__menu-open"),
-open = document.querySelector(".header__menu-close");
+open = document.querySelector(".header__menu-close"),
+modal = document.querySelector(".modal"),
+modalOpen = document.querySelector(".goods__button"),
+modalClose = document.querySelector(".modal__button");
 
 nav.classList.remove("navigation--open");
 close.classList.add("header__menu-toggle--hidden");
@@ -12,4 +15,14 @@ toggle.addEventListener("click", function(e) {
 	nav.classList.toggle("navigation--open");
 	close.classList.toggle("header__menu-toggle--hidden");
 	open.classList.toggle("header__menu-toggle--hidden");
+})
+
+modalOpen.addEventListener("click", function(e) {
+	e.preventDefault();
+	modal.classList.add("modal--open");
+})
+
+modalClose.addEventListener("click", function(e) {
+	e.preventDefault();
+	modal.classList.remove("modal--open");
 })
