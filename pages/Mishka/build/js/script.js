fcbf254d7c5674d312dbe2b9e3415b74106f9fd0
Victor <victormuryn24@gmail.com>
2019-05-05 19:24:34 +0300

    savE

--- pages/Mishka/build/js/script.js
+++ pages/Mishka/build/js/script.js
@@ -4,7 +4,8 @@ close = document.querySelector(".header__menu-open"),
 open = document.querySelector(".header__menu-close"),
 modal = document.querySelector(".modal"),
 modalOpen = document.querySelector(".goods__button"),
-modalClose = document.querySelector(".modal__button");
+modalClose = document.querySelector(".modal__button"),
+parallaxText = document.querySelector(".main__text");
 
 nav.classList.remove("navigation--open");
 close.classList.add("header__menu-toggle--hidden");
@@ -26,3 +27,8 @@ modalClose.addEventListener("click", function(e) {
 	e.preventDefault();
 	modal.classList.remove("modal--open");
 })
+
+window.onscroll = function() {
+  let scrolled = window.pageYOffset || document.documentElement.scrollTop;
+  parallaxText.style.transform = "translateY(-" + scrolled / 3 + "px)";
+}
